609bdf3ab22fd8dc974f835fd3419a80d7e0e5a5
mttaat <mttaat@protonmail.ch>
2019-02-13 19:19:50 -0800

    Stub for pathology.py

---
+++ README.md
@@ -0,0 +1,2 @@
+# pathology
+A web path fuzzer in the spirit of dirbuster with support for multiple concurrent domains, arbitrary file extensions and user-definable success/failure criteria based on response page text.
---
+++ pathology.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2019 mttaat <mttaat@protonmail.com>. All Rights Reserved.
+# This file licensed under the GPLv3
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# pathology v0.1
+# path-based web fuzzer
+#
+# usage: $ python pathology -d <domains,domains> -w <wordlist> -s '<success regex>' -f '<fail regex>' -x <file extension,file extension> -o <outputfile>
+#
+# # # # # # # # 
+#
+# @mttaat
+# https://mttaat.net
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+
